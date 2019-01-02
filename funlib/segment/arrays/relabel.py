b2f62c964917597da74d9132cd93272e59b2a16c
Jan Funke <jfunke@iri.upc.edu>
2019-01-02 06:45:25 -0500

    Return number of unique labels (!=0) in relabel

--- funlib/segment/arrays/relabel.py
+++ funlib/segment/arrays/relabel.py
@@ -4,7 +4,38 @@ import numpy as np
 
 
 def relabel(array, return_backwards_map=False, inplace=False):
-    '''Relabel array, such that IDs are consecutive. Excludes 0.'''
+    '''Relabel array, such that IDs are consecutive. Excludes 0.
+
+    Args:
+
+        array (ndarray):
+
+                The array to relabel.
+
+        return_backwards_map (``bool``, optional):
+
+                If ``True``, return an ndarray that maps new labels (indices in
+                the array) to old labels.
+
+        inplace (``bool``, optional):
+
+                Perform the replacement in-place on ``array``.
+
+    Returns:
+
+        A tuple ``(relabelled, n)``, where ``relabelled`` is the relabelled
+        array and ``n`` the number of unique labels found.
+
+        If ``return_backwards_map`` is ``True``, returns ``(relabelled, n,
+        backwards_map)``.
+    '''
+
+    if array.size == 0:
+
+        if return_backwards_map:
+            return array, 0, []
+        else:
+            return array, 0
 
     # get all labels except 0
     old_labels = np.unique(array)
@@ -13,12 +44,12 @@ def relabel(array, return_backwards_map=False, inplace=False):
     if old_labels.size == 0:
 
         if return_backwards_map:
-            return array, 1, [0]
+            return array, 0, [0]
         else:
-            return array, 1
+            return array, 0
 
-    n = len(old_labels) + 1
-    new_labels = np.arange(1, n, dtype=array.dtype)
+    n = len(old_labels)
+    new_labels = np.arange(1, n + 1, dtype=array.dtype)
 
     replaced = replace_values(array, old_labels, new_labels, inplace=inplace)
 
